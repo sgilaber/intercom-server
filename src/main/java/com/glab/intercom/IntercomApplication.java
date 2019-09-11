30951af57cae97e9b4ec0f0c89b6745f742cd8c4
Sébastien Gilabert <sebastien.gilabert@inria.fr>
2019-09-11 15:26:39 +0200

    initialize

---
+++ src/main/java/com/glab/intercom/IntercomApplication.java
@@ -0,0 +1,13 @@
+package com.glab.intercom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glab/intercom/IntercomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glab.intercom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
